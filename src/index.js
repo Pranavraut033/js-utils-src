aeae4d04bc57d08a36df2786b2add4e47b28b4ed
Pranav Raut <pranavraut033@gmail.com>
2020-08-19 11:16:31 +0530

    updates
    Signed-off-by: Pranav Raut <pranavraut033@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-export const date = require("./utils/date");
+// export const date = require("./utils/date");
+
 export const list = require("./utils/list");
+
 export const object = require("./utils/object");
+
 export const regex = require("./utils/regex");
+
 export const AxiosHelper = require("./utils/AxiosHelper");
+
 export const FireStorageHelper = require("./utils/FireStorageHelper");
+
 export const navigation = require("./utils/navigation");
--- src/utils/date.js
+++ src/utils/date.js
@@ -1,26 +1,26 @@
-import moment from "moment";
+// import moment from "moment";
 
-/**
- *
- * @param {Date|String|Number} date Date source
- */
-export function getDate(date, getTime = false, format = "DD MMMM YYYY") {
-  var src = new Date(date);
-  var diff = dateDiff(src, new Date());
+// /**
+//  *
+//  * @param {Date|String|Number} date Date source
+//  */
+// export function getDate(date, getTime = false, format = "DD MMMM YYYY") {
+//   var src = new Date(date);
+//   var diff = dateDiff(src, new Date());
 
-  switch (diff) {
-    case 0:
-      return getTime ? moment(src).format("LT") : "Today";
-    case 1:
-      return "Yesterday";
-    default:
-      return moment(src).format(format);
-  }
-}
+//   switch (diff) {
+//     case 0:
+//       return getTime ? moment(src).format("LT") : "Today";
+//     case 1:
+//       return "Yesterday";
+//     default:
+//       return moment(src).format(format);
+//   }
+// }
 
-export function dateDiff(d1, d2) {
-  var date1 = new Date(new Date(d1).toISOString().slice(0, 10)),
-    date2 = new Date(new Date(d2).toISOString().slice(0, 10));
+// export function dateDiff(d1, d2) {
+//   var date1 = new Date(new Date(d1).toISOString().slice(0, 10)),
+//     date2 = new Date(new Date(d2).toISOString().slice(0, 10));
 
-  return (date1 - date2) / 86400000;
-}
+//   return (date1 - date2) / 86400000;
+// }
