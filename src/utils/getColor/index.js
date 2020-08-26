a7ce32fe83c05da961fabb5e6ffb96ebc1b3121f
Pranav Raut <pranavraut033@gmail.com>
2020-08-26 22:47:19 +0530

    updates
    Signed-off-by: Pranav Raut <pranavraut033@gmail.com>

--- src/utils/getColor/index.js
+++ src/utils/getColor/index.js
@@ -8,3 +8,10 @@ let colorMap = {};
 export default function (string) {
   return colorMap[string] || (colorMap[string] = stc(string));
 }
+
+/**
+ *
+ * @param {String} text
+ * @param {String} color hex color
+ */
+export const setColor = (text, color) => (colorMap[text] = color);
